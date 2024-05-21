6934ae945598dde9b361c7db91d22b18a775093d
Mr.GeeKy <mr.geeky1992@gmail.com>
2024-05-21 15:16:25 +0430

    Use Host Binding & Listener

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -1,15 +1,22 @@
-import { Directive,ElementRef,Renderer2 } from '@angular/core';
+import { Directive,ElementRef,Renderer2,HostBinding,HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appClass]',
   standalone: true
 })
 export class ClassDirective {
-
+  @HostBinding('style.backgroundColor') backgroundColor : string ='transparent';
   constructor(private elementref : ElementRef , private renderer : Renderer2) {
     //this.elementref.nativeElement.style.backgroundcolor = 'red';
-    this.renderer.setStyle(this.elementref.nativeElement,'background-color' , 'red');
+    //this.renderer.setStyle(this.elementref.nativeElement,'background-color' , 'red');
     //console.log("its test");
    }
+  @HostListener('mouseenter') inmouseover(){
+    //this.renderer.setStyle(this.elementref.nativeElement,'background-color' , 'yellow');
+    this.backgroundColor ='yellow';
+  }
+  @HostListener('mouseleave') onMouseOut(){
+    this.backgroundColor='transparent';
+  }
 
 }
