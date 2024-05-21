dcb56bf549c97f3427542acad358a5cf91e93509
Mr.GeeKy <mr.geeky1992@gmail.com>
2024-05-21 16:14:25 +0430

    get data from parent componenet

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -1,22 +1,27 @@
-import { Directive,ElementRef,Renderer2,HostBinding,HostListener} from '@angular/core';
+import { Directive,ElementRef,Renderer2,HostBinding,HostListener ,input, Input} from '@angular/core';
 
 @Directive({
   selector: '[appClass]',
   standalone: true
 })
 export class ClassDirective {
-  @HostBinding('style.backgroundColor') backgroundColor : string ='transparent';
+  //@HostBinding('style.backgroundColor') backgroundColor : string ='transparent';
+  @Input()set backgroundColor(color : string){
+    this.elementref.nativeElement.style.backgroundColor=color;
+  }
   constructor(private elementref : ElementRef , private renderer : Renderer2) {
     //this.elementref.nativeElement.style.backgroundcolor = 'red';
     //this.renderer.setStyle(this.elementref.nativeElement,'background-color' , 'red');
     //console.log("its test");
+
+
    }
   @HostListener('mouseenter') inmouseover(){
     //this.renderer.setStyle(this.elementref.nativeElement,'background-color' , 'yellow');
-    this.backgroundColor ='yellow';
+    //this.backgroundColor ='yellow';
   }
   @HostListener('mouseleave') onMouseOut(){
-    this.backgroundColor='transparent';
+    //this.backgroundColor='transparent';
   }
 
 }
