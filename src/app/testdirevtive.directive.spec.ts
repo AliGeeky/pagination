d014efe7fa6d2902bad4e6ffc0241b9211ba9db1
Mr.Geeky <mr.geeky1992@gmail.com>
2024-05-22 19:35:01 +0330

    use [ngStyle]

--- src/app/testdirevtive.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TestdirevtiveDirective } from './testdirevtive.directive';
-
-describe('TestdirevtiveDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TestdirevtiveDirective();
-    expect(directive).toBeTruthy();
-  });
-});
